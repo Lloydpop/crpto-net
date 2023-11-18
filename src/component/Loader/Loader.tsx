aa3588c414f7fd4d3e3f8b672783c1e411f76922
lloydpop <lloydperfect199@gmail.com>
2023-11-18 11:55:31 +0100

    'updated

--- src/component/Loader/Loader.tsx
+++ src/component/Loader/Loader.tsx
@@ -5,7 +5,7 @@ interface LoaderProps {
 
 export const Loader = ({ title }: LoaderProps) => {
   return (
-    <div className="flex items-center flex-col justify-center lg:min-h-[400px] lg:mt-0 mt-24">
+    <div className="flex items-center flex-col justify-center lg:min-h-[400px] lg:mt-0 mt-12">
       <img src={bit} alt="loading" className="w-[160px]" />
       <p className="font-titi font-bold text-[20px]">{title}</p>
     </div>
